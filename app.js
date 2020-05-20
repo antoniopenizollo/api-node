c259c754050aab6b5f1089a3588442ec0e4c6a3b
antoniopenizollo <antoniopenizollo@gmail.com>
2020-05-20 18:56:08 -0300

    criando conexao com banco

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const indexRoute = require('./routes/index');
-const usersRoutes = require('./routes/users')
+const url = 'mongodb+srv://usuario_admin:nZJuzAeohOx7td9O@clusterapi-0igpf.mongodb.net/test?retryWrites=true&w=majority';
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
 
-app.use('/',indexRoute);
-app.use('/users',usersRoutes);
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
 
+mongoose.connection.on('error', (err) => {
+    console.log('Erro na conexão com o banco de dados: ' + err);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do banco de dados!');
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada ao banco de dados!');
+})
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const indexRoute = require('./Routes/index');
+const usersRoute = require('./Routes/users');
+
+app.use('/', indexRoute);
+app.use('/users', usersRoute);
 
 app.listen(3000);
 
-module.exports = app; 
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => {
-    return res.send({message: 'Tudo ok com o método GET do usuario'});
-})
+router.get('/', (req, res) => {
+    return res.send({message: 'Tudo ok com o método GET da rota de usuários!'});
+});
 
-router.post('/',(req,res) => {
-    return res.send({message: 'Tudo ok com o método POST do usuario'});
-})
+router.post('/', (req, res) => {
+    return res.send({message: 'Tudo ok com o método POST da rota de usuários!'});
+});
 
-router.post('/create',(req,res) => {
-    return res.send({message: 'usuário criado com sucesso'});
-})
+router.post('/create', (req, res) => {
+    
+    return res.send({message: 'Seu usuário foi criado =)'});
+});
 
 module.exports = router;
