ef62e57c2d313c260b28cedee894b32588f998f1
antoniopenizollo <antoniopenizollo@gmail.com>
2020-05-20 20:43:27 -0300

    autenticação com jwt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+//funcões auxiliares
+//criação de senha provisória
+const createUserToken = (userId) => {
+    return jwt.sign({id : userId}, 'antonio2020',{expiresIn: '7d'});
+}
+
 
 
 router.get('/', async(req, res) => {
@@ -25,7 +33,8 @@ router.post('/create', async(req,res)=>{
 
         const user = await Users.create(req.body);
         user.password = undefined;
-        return res.send(user);
+
+        return res.send({user,token: createUserToken(user.id)});
     }
     catch(err){
         return res.send({error : 'Erro ao buscar usuário'});
@@ -47,7 +56,7 @@ router.post('/auth',async (req,res) => {
 
         user.password = undefined;
 
-        return res.send(user);
+        return res.send({user,token: createUserToken(user.id)});
     }
     catch(err){
         return res.send({ error : 'Erro ao criar usuário'});
